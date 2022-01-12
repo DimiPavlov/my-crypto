1f6a1fc234138158b1baa5c07cf7357618ae922c
Dimitar Pavlov <dpavlov@devexperts.com>
2022-01-12 20:01:37 +0200

    add react-router, create routes for every page and include layout

--- src/App.js
+++ src/App.js
@@ -1,9 +1,55 @@
 import React from "react";
+import { Routes, Link, Route } from "react-router-dom";
+import { Layout, Typography, Space } from "antd";
+import {
+  Navbar,
+  Exchanges,
+  Homepage,
+  Cryptocurrencies,
+  News,
+  CryptoDetails,
+} from "./components";
+import "./App.css";
 
 const App = () => {
   return (
-    <div>
-      <h1>Wellcome React</h1>
+    <div className="app">
+      <div className="navbar">
+        <Navbar />
+      </div>
+      <div className="main">
+        <Layout>
+          <div className="routes">
+            <Routes>
+              <Route exact path="/" element={<Homepage />} />
+              <Route exact path="/exchanges" element={<Exchanges />} />
+              <Route
+                exact
+                path="/cryptocurrencies"
+                element={<Cryptocurrencies />}
+              />
+              <Route exact path="/news" element={<News />} />
+              <Route exact path="/crypto/:coinId" element={<CryptoDetails />} />
+            </Routes>
+          </div>
+        </Layout>
+
+        <div className="footer">
+          <Typography.Title
+            level={5}
+            style={{ color: "wheat", textAlign: "center" }}
+          >
+            CryptoVerse
+            <br />
+            All rights reserved
+          </Typography.Title>
+          <Space>
+            <Link to="/">Homepage</Link>
+            <Link to="/exchanges">Exchanges</Link>
+            <Link to="/news">News</Link>
+          </Space>
+        </div>
+      </div>
     </div>
   );
 };
---
+++ src/components/Cryptocurrencies.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Cryprocurrencies = () => {
+  return <div>Cryprocurrencies</div>;
+};
+
+export default Cryprocurrencies;

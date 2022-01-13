e21bf8b3dd93b11801718c5c031b69b9a0a15697
Dimitar Pavlov <dpavlov@devexperts.com>
2022-01-13 18:31:48 +0200

    display Cryptocurrencies  and clean unused code

--- src/components/Cryptocurrencies.jsx
+++ src/components/Cryptocurrencies.jsx
@@ -1,7 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
+
+import millify from "millify";
+import { Row, Col, Input, Card } from "antd";
+import { Link } from "react-router-dom";
+
+import { useGetCryptosQuery } from "../services/cryptoApi";
 
 const Cryprocurrencies = () => {
-  return <div>Cryprocurrencies</div>;
+  const { data: cryptosList, isLoading } = useGetCryptosQuery();
+  const [cryptos, setCryptos] = useState(cryptosList.data?.coins);
+  console.log(cryptos);
+
+  return (
+    <>
+      <Row gutter={[32, 32]} className="crypto-card-container">
+        {cryptos.map((currency) => (
+          <Col xs={32} sm={12} lg={6} className="crypto-card" key={currency.id}>
+            <Link to={`/crypto/${currency.id}`}>
+              <Card
+                title={`${currency.rank}. ${currency.name}`}
+                extra={<img className="crypto-image" src={currency.iconUrl} />}
+                hoverable
+              >
+                <p>Price: {millify(currency.price)}</p>
+                <p>Market Cap: {millify(currency.marketCap)}</p>
+                <p>Daily Change: {millify(currency.change)}%</p>
+              </Card>
+            </Link>
+          </Col>
+        ))}
+      </Row>
+    </>
+  );
 };
 
 export default Cryprocurrencies;
--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -19,21 +19,3 @@ export const cryptoApi = createApi({
 });
 
 export const { useGetCryptosQuery } = cryptoApi;
-
-// var options = {
-//     method: 'GET',
-//     url: 'https://coinranking1.p.rapidapi.com/coins',
-//     params: {
-//       referenceCurrencyUuid: 'yhjMzLPhuIDl',
-//       timePeriod: '24h',
-//       tiers: '1',
-//       orderBy: 'marketCap',
-//       orderDirection: 'desc',
-//       limit: '50',
-//       offset: '0'
-//     },
-//     headers: {
-//       'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
-//       'x-rapidapi-key': '59942876damshfc39b47f9e96c43p1fbd31jsn28b3ba9cc905'
-//     }
-//   };
